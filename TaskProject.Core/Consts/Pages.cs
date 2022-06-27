caa83a05e6e21d67f0153df98a65ad4d3f6ddc19
Elmar Fazlagic <elmar.fazlagic@AuthorityPartners.com>
2022-06-28 01:10:36 +0200

    Login Page Implementation

---
+++ TaskProject.Core/Consts/Pages.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskProject.Core.Consts
+{
+    public class Pages
+    {
+        public const string NavigationPage = nameof(NavigationPage);
+
+        public const string LoginPage = nameof(LoginPage);
+
+        public const string HomePage = nameof(HomePage);
+    }
+}
