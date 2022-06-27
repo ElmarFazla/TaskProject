982e4b68ab09a95c7e485906cf21f606d410bf36
Elmar Fazlagic <elmar.fazlagic@AuthorityPartners.com>
2022-06-27 23:11:07 +0200

    Add project files.

---
+++ TaskProject.Core/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Prism.Navigation;
+
+namespace TaskProject.Core.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        public MainPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            Title = "Home Page";
+        }
+    }
+}
---
+++ TaskProject/TaskProject/Views/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskProject.Views
+{
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
